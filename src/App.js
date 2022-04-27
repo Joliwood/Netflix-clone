17dc05bc4a26fbea4f4c971551e3eba3ca07deb8
Joliwood <jolibois.contact@gmail.com>
2022-04-27 19:27:02 +0200

    27/04 : Fin de session

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React from "react";
 import './App.css';
 import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <p>oh hello there</p>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
 
     </div>
   );
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "./axios";
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+  const[movies, setMovies] = useState([]);
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results)
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
   return (
-    <div>
+    <div className="row">
         <h2>{title}</h2>
+
+        <div className="row__posters">
+          {/* container -> posters */}
+          {movies.map(movie => (
+            <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          ))}
+        </div>
     </div>
   )
 };

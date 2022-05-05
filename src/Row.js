97aadb6aae9fe11b7746e7be7bfb2e0665ee82fe
Joliwood <jolibois.contact@gmail.com>
2022-05-05 20:15:16 +0200

    05/05 : Fin de session, navbar terminée, bugs à résoudre pour prochaine session

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const[movies, setMovies] = useState([]);
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -15,19 +16,29 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVar: {
+      
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className="row">
         <h2>{title}</h2>
 
         <div className="row__posters">
-          {/* container -> posters */}
           {movies.map(movie => (
             <img
               key={movie.id}
-              className="row__poster" 
-              src={`${base_url}${movie.poster_path}`} alt={movie.name} 
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+              alt={movie.name} 
             />
           ))}
+          {/* <YouTube videoId="https://www.youtube.com/watch?v=rpIuoxMSFeQ" opts={opts} /> */}
         </div>
     </div>
   )

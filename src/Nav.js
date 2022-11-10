081f00c30befe853ea1cf43ebfe77a83fb701a00
Joliwood <jolibois.contact@gmail.com>
2022-11-10 10:15:35 +0100

    :zap: test for private repo host

--- src/Nav.js
+++ src/Nav.js
@@ -21,7 +21,8 @@ function Nav() {
             className="nav__logo"
             src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
             alt="Netflix Logo"
-        />
+            />
+            <p>ok boomer</p>
 
         <img 
             className="nav__avatar"
